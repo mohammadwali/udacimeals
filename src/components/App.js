72d34007ff4ba62978753433c74b80a31fee33dd
Mohammad Wali <mohdwali@live.com>
2017-08-28 12:18:04 +0530

    Testing mapDispatchToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { addRecipe, removeFromCalendar } from '../actions'
 
 class App extends Component {
     render() {
+        console.log(this.props)
         return (
             <div>
                 Hello World
@@ -28,6 +30,14 @@ function mapStateToProps (calendar) {
     }
 }
 
+function mapDispatchToProps (dispatch) {
+    return {
+        selectRecipe: (data) => dispatch(addRecipe(data)),
+        remove: (data) => dispatch(removeFromCalendar(data))
+    }
+}
+
 export default connect(
     mapStateToProps,
+    mapDispatchToProps
 )(App)
